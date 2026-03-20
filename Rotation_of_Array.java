196e6b94c8b8f6ab826817734db9e46ed2a00b6e
SrivarshiniRajeshkanna <varshudpm09@gmail.com>
2026-03-20 18:52:43 +0530

    ADDED ADDITIONAL SUMS

---
+++ Rotation_of_Array.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class Rotation_of_Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of matrix (n x n): ");
+        int n = sc.nextInt();
+        int[][] matrix = new int[n][n];
+        System.out.println("Enter matrix elements:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = temp;
+            }
+        }
+        System.out.println("Rotated Matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
